bfad2a55eac7c9056892b8faed23a39d2c20b051
d.kantorowicz@odo24.pl <kotorozec.code@gmail.com>
2024-01-22 13:28:41 +0100

    Types

--- Content.md
+++
@@ -1,3 +0,0 @@
-# Content 
-- [Primitives types](https://github.com/Chomikens/ZTM-JS/blob/1-types/types/types.md)
-- [Compations](https://github.com/Chomikens/ZTM-JS/blob/1-types/types/types.md)
---
+++ exercise-first/app-solutions.js
@@ -0,0 +1,36 @@
+//Evaluate the below:
+5 + "34" // 534
+5 - "4" // 1
+10 % 5 // 2
+5 % 10 //5
+"Java" + "Script" // "JAva Script"
+" " + " " // "  "
+" " + 0 // " 0"
+
+// Why this will work ? JS automaticly will change thise value to 1(true) and 0(false)
+true + true // 2
+true + false // 1
+false + true // 1
+false - true // -1
+3 - 4 // -1 
+"Bob" - "bill" //Nan
+
+
+//Evaluate the below comparisons:
+5 >= 1 // true
+0 === 1 // false
+4 <= 1 //false
+1 != 1 // false
+
+// Why this will work? //  The ASCII value of "A" is 65. The ASCII value of "a" is 98.
+
+"A" > "B" // false
+"B" < "C" // true
+"a" > "A" // true
+"b" < "A" / true
+true === false  // false
+true != true // false
+
+
+// Make the string: "Hi There! It's "sunny" out" by using the + sign:
+
