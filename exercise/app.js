8bc38e2deb8dbef3685493bbc03f6611e6033ca5
d.kantorowicz@odo24.pl <kotorozec.code@gmail.com>
2024-01-24 09:19:28 +0100

    Exercise done

---
+++ exercise/app.js
@@ -0,0 +1,16 @@
+// Create an object and an array which we will use in our facebook exercise. 
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
+// 2. Create an array which contains the object you have made above and name the array "database".
+// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+
+const basicUser = { 
+    name: "Dominik",
+    lastName: "Kantorowicz",
+}
+
+const arrayOfUsers = []
+
+arrayOfUsers.push(basicUser)
+
+const newsfeed = [{userName: null, timeLine:null}, {userName: null, timeLine:null}, {userName: null, timeLine:null},]
+
