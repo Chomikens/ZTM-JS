32b4d35e66ac265afe6fd0fed8f599c19626e9a8
d.kantorowicz@odo24.pl <kotorozec.code@gmail.com>
2024-02-08 09:31:30 +0100

    Exercise part II

--- exercise/advance-objects/app.js
+++ exercise/advance-objects/app.js
@@ -13,3 +13,28 @@ object1.a = 4;
 
 //#3 create two classes: an Animal class and a Mamal class. 
 // create a cow that accepts a name, type and color and has a sound method that moo's her name, type and color. 
+
+
+class Animal { 
+    constructor (animalName, animaType) {
+        this.animalName = animalName; 
+        this.animaType = animaType; 
+    }
+    baseInfo() {
+        console.log(`I'm ${this.animalName} and ${this.animaType}`)
+    }
+}
+
+class Mamal extends Animal {
+    constructor (animalName, animaType, animalColor) {
+        super (nimalName, animaType)
+        this.animalColor = animalColor;
+    }
+    super(baseInfo) {
+        console.log(`I'm ${this.animalName} and ${this.animaType} also I'm this color: ${this.animalColor}`)
+    }
+}
+
+const cow = new Mamal("Kotorozec", "Cow", "black")
+
+console.log(cow, cow.baseInfo())
